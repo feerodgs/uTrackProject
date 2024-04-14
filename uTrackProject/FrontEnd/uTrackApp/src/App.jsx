424395db2cdb9e345f738f55cb5fa184a5a073eb
DESKTOP-K5Q8CPT\Nathan <101741@aluno.uricer.edu.br>
2024-04-14 16:20:59 -0300

    Alterações de cor e animação form

--- uTrackProject/FrontEnd/uTrackApp/src/App.jsx
+++ uTrackProject/FrontEnd/uTrackApp/src/App.jsx
@@ -1,12 +1,8 @@
-import styles from './App.module.css'
-
+import React, { useState, useEffect } from 'react';
+import styles from './App.module.css';
 import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
-
 import '@aws-amplify/ui-react/styles.css';
 import { fetchAuthSession } from 'aws-amplify/auth';
-
-import { useState, useEffect } from 'react';
-
 import { I18n } from 'aws-amplify/utils';
 import { translations } from '@aws-amplify/ui-react';
 
@@ -39,16 +35,16 @@ const formFields = {
   }
 }
 
-
 function App() {
   const { route } = useAuthenticator(context => [context.route]);
   const [accessToken, setAccessToken] = useState(null);
   const [idToken, setIdToken] = useState(null);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   useEffect(() => {
     const currentSession = async () => {
       try {
-        const session = await fetchAuthSession(); {/* A session pega os tokens de autenticação do usuário quando ele efetua o login */ }
+        const session = await fetchAuthSession();
         setAccessToken(session.tokens.accessToken.toString());
         setIdToken(session.tokens.idToken.toString());
       } catch (err) {
@@ -59,32 +55,32 @@ function App() {
     currentSession();
   }, []);
 
+  const toggleExpansion = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   return (
     <div className={styles.App}>
-      <img className={styles.logo} src="/vite.svg" alt="Logotipo uTrack" /> {/* basta colocar a logo na pasta public e chamar ela com /nomeDoArquivo.svg */}
-      {route !== 'authenticated' && <h1 className='h1-bold'>uTrack</h1>} {/* Renderiza o componente com o usuário deslogado. */}
+      <img className={styles.logo} src="/logo.png" alt="Logotipo uTrack" />
       <div className={styles.letter}>
-        <Authenticator className={styles.loginForm} formFields={formFields} >
-          {({ signOut, user }) => (
-            <div className={styles.container}>
-              {/* Aqui podemos renderizar os componentes que irão compor o App */}
-              <p>
-                Olá {user.username}, bem vindo ao uTrack. {/* Cada user tem um user.username único, útil para salvar no banco de dados como a identificação de cada user. */}
-              </p>
-              {/* O email do usuário pode ser acessado em {user.signInDetails.loginId} */}
-              <p>Email: {user.signInDetails.loginId}</p>
-              {/*
-                <p>{idToken}</p>
-                <p>{accessToken}</p> {/* Tokens de sessão do usuário, usados para mandar como uma "prova" de que o usuário está autenticado para as apis */}
-              <button onClick={signOut}>Sair</button>
-            </div>
-          )}
-        </Authenticator>
-        {route !== 'authenticated' && <div className={styles.letterdown}> A ideia é essa, ai vcs se viram</div>}
+        <div className={`${styles.loginForm} ${isExpanded ? styles.expanded : ''}`}>
+          <Authenticator formFields={formFields}>
+            {({ signOut, user }) => (
+              <div className={styles.container}>
+                <p>
+                  Olá {user.username}, bem vindo ao uTrack.
+                </p>
+                <p>Email: {user.signInDetails.loginId}</p>
+                <button onClick={signOut}>Sair</button>
+              </div>
+            )}
+          </Authenticator>
+        </div>
+        {route !== 'authenticated' && <div className={styles.letterdown} onClick={toggleExpansion}></div>}
+        {route !== 'authenticated' && <div className={styles.letterfront}></div>}
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
