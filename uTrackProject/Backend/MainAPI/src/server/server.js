3a2946b7e4134496c2317ebcf1755a0ce2901c71
felipe <101447@aluno.uricer.edu.br>
2024-06-13 20:53:24 -0300

    Adicionado validação de cors.

--- uTrackProject/Backend/MainAPI/src/server/server.js
+++ uTrackProject/Backend/MainAPI/src/server/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import tracksController from '../controller/tracks.js';
 import userController from '../controller/users.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (request, response) => {
